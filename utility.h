0a4757c60641dda9d3b540426f4c94cd4db4169c
gabripian <gabrielepianigiani28@gmail.com>
2024-05-08 23:02:46 +0200

    Update utility.h

--- utility.h
+++ utility.h
@@ -5,3 +5,16 @@ using namespace std;
 bool checkEmailFormat(string emailRecv){
     return true;
 }
+
+void insertUserInFile(string userString){
+    
+    string filename("tmp.txt");
+    fstream userFile;
+    userFile.open(filename, std::fstream::app);
+    
+    if (userFile.is_open()) {
+        userFile << userString << endl;
+    } else {
+        cout << "Error during the file opening" << endl;
+    }
+}
